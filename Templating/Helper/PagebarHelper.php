9c552d76a0660e33e2018ca0835a49ba27df22f7
javiacei <javier.aceituno@ideup.com>
2011-09-21 16:36:37 +0200

    complete basic render. simple_paginator_render

--- Templating/Helper/PagebarHelper.php
+++ Templating/Helper/PagebarHelper.php
@@ -2,10 +2,11 @@
 
 namespace Ideup\SimplePaginatorBundle\Templating\Helper;
 
-use Symfony\Component\Templating\Helper\Helper;
-use Symfony\Component\Templating\EngineInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
-use Ideup\SimplePaginatorBundle\Paginator\Paginator as Paginator;
+use 
+    Symfony\Component\Templating\Helper\Helper,
+    Symfony\Component\DependencyInjection\ContainerInterface,
+    Ideup\SimplePaginatorBundle\Paginator\Paginator as Paginator
+;
 
 class PagebarHelper extends Helper
 {
@@ -19,101 +20,116 @@ class PagebarHelper extends Helper
         $this->container = $container;
     }
 
-    public function first($routeName, $title, $options, $paginatorId)
-    {
-        $template = 'IdeupSimplePaginatorBundle:Paginator:first.html.twig';
-        $title = (is_null($title)) ? '<< First' : $title;
-        return trim($this->container->get('templating')->render($template, array(
-                    'paginator' => $this->paginator,
-                    'routeName' => (is_null($routeName)) ? '' : $routeName,
-                    'title' => $title,
-                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
-                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
-                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
-                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
-                )));
-    }
+//    public function first($routeName, $title, $options, $paginatorId)
+//    {
+//        $template = 'IdeupSimplePaginatorBundle:Paginator:first.html.twig';
+//        $title = (is_null($title)) ? '<< First' : $title;
+//        return trim($this->container->get('templating')->render($template, array(
+//                    'paginator' => $this->paginator,
+//                    'routeName' => (is_null($routeName)) ? '' : $routeName,
+//                    'title' => $title,
+//                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
+//                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
+//                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
+//                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
+//                )));
+//    }
+//
+//    public function prev($routeName, $title, $options, $paginatorId)
+//    {
+//        $template = 'IdeupSimplePaginatorBundle:Paginator:prev.html.twig';
+//        $title = (is_null($title)) ? '< Prev' : $title;
+//        return trim($this->container->get('templating')->render($template, array(
+//                    'paginator' => $this->paginator,
+//                    'routeName' => (is_null($routeName)) ? '' : $routeName,
+//                    'title' => $title,
+//                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
+//                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
+//                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
+//                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
+//                )));
+//    }
+//
+//    public function numbers($routeName, $options, $paginatorId)
+//    {
+//        $template = 'IdeupSimplePaginatorBundle:Paginator:numbers.html.twig';
+//        return trim($this->container->get('templating')->render($template, array(
+//                    'paginator' => $this->paginator,
+//                    'routeName' => (is_null($routeName)) ? '' : $routeName,
+//                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
+//                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
+//                    'currentClass' => (isset($options['currentClass'])) ? $options['currentClass'] : 'current',
+//                    'modulus' => (isset($options['modulus'])) ? $options['modulus'] : 8,
+//                    'first' => (isset($options['first'])) ? $options['first'] : 0,
+//                    'last' => (isset($options['last'])) ? $options['last'] : 0,
+//                    'separator' => (isset($options['separator'])) ? $options['separator'] : '|'
+//                )));
+//    }
+//
+//    public function next($routeName, $title, $options, $paginatorId)
+//    {
+//        $template = 'IdeupSimplePaginatorBundle:Paginator:next.html.twig';
+//        $title = (is_null($title)) ? 'Next >' : $title;
+//        return trim($this->container->get('templating')->render($template, array(
+//                    'paginator' => $this->paginator,
+//                    'routeName' => (is_null($routeName)) ? '' : $routeName,
+//                    'title' => $title,
+//                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
+//                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
+//                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
+//                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
+//                )));
+//    }
+//
+//    public function last($routeName, $title, $options, $paginatorId)
+//    {
+//        $template = 'IdeupSimplePaginatorBundle:Paginator:last.html.twig';
+//        $title = (is_null($title)) ? 'Last >>' : $title;
+//        return trim($this->container->get('templating')->render($template, array(
+//                    'paginator' => $this->paginator,
+//                    'routeName' => (is_null($routeName)) ? '' : $routeName,
+//                    'title' => $title,
+//                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
+//                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
+//                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
+//                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
+//                )));
+//    }
+//
+//    public function counter($format, $paginatorId)
+//    {
+//        $paginatorId = (is_null($paginatorId)) ? '' : $paginatorId;
+//        $format = (is_null($format)) ? 'Page %page% of %pages%, showing %current% records out of %count% total, starting on record %start%, ending on %end%' : $format;
+//        $fCounter = str_replace('%page%', $this->paginator->getCurrentPage($paginatorId), $format);
+//        $fCounter = str_replace('%pages%', $this->paginator->getLastPage($paginatorId), $fCounter);
+//        $fCounter = str_replace('%current%', $this->paginator->getItemsPerPage($paginatorId), $fCounter);
+//        $fCounter = str_replace('%count%', $this->paginator->getTotalItems($paginatorId), $fCounter);
+//        $fCounter = str_replace('%start%', $this->paginator->getOffset($paginatorId) + 1, $fCounter);
+//        $end = $this->paginator->getOffset($paginatorId) + $this->paginator->getItemsPerPage($paginatorId);
+//        $end = ($end < $this->paginator->getTotalItems($paginatorId)) ? $end : $this->paginator->getTotalItems($paginatorId);
+//        $fCounter = str_replace('%end%', $end, $fCounter);
+//        return trim($fCounter);
+//    }
+//
+//    public function offset($index=0, $paginatorId, $startIndex)
+//    {
+//        $paginatorId = (is_null($paginatorId)) ? '' : $paginatorId;
+//        return $this->paginator->getOffset($paginatorId) + $index + $startIndex;
+//    }
 
-    public function prev($routeName, $title, $options, $paginatorId)
+    public function render($id, $route, $options = array(), $view = null)
     {
-        $template = 'IdeupSimplePaginatorBundle:Paginator:prev.html.twig';
-        $title = (is_null($title)) ? '< Prev' : $title;
-        return trim($this->container->get('templating')->render($template, array(
-                    'paginator' => $this->paginator,
-                    'routeName' => (is_null($routeName)) ? '' : $routeName,
-                    'title' => $title,
-                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
-                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
-                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
-                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
-                )));
-    }
+        $view = (!is_null($view)) ? $view : 'IdeupSimplePaginatorBundle:Paginator:paginator.html.twig';
 
-    public function numbers($routeName, $options, $paginatorId)
-    {
-        $template = 'IdeupSimplePaginatorBundle:Paginator:numbers.html.twig';
-        return trim($this->container->get('templating')->render($template, array(
-                    'paginator' => $this->paginator,
-                    'routeName' => (is_null($routeName)) ? '' : $routeName,
-                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
-                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
-                    'currentClass' => (isset($options['currentClass'])) ? $options['currentClass'] : 'current',
-                    'modulus' => (isset($options['modulus'])) ? $options['modulus'] : 8,
-                    'first' => (isset($options['first'])) ? $options['first'] : 0,
-                    'last' => (isset($options['last'])) ? $options['last'] : 0,
-                    'separator' => (isset($options['separator'])) ? $options['separator'] : '|'
-                )));
-    }
+        $defaultOptions = array(
+            'paginator' => $this->paginator,
+            'id' => $id,
+            'route' => $route
+        );
 
-    public function next($routeName, $title, $options, $paginatorId)
-    {
-        $template = 'IdeupSimplePaginatorBundle:Paginator:next.html.twig';
-        $title = (is_null($title)) ? 'Next >' : $title;
-        return trim($this->container->get('templating')->render($template, array(
-                    'paginator' => $this->paginator,
-                    'routeName' => (is_null($routeName)) ? '' : $routeName,
-                    'title' => $title,
-                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
-                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
-                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
-                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
-                )));
-    }
+        $options = \array_merge($defaultOptions, $options);
 
-    public function last($routeName, $title, $options, $paginatorId)
-    {
-        $template = 'IdeupSimplePaginatorBundle:Paginator:last.html.twig';
-        $title = (is_null($title)) ? 'Last >>' : $title;
-        return trim($this->container->get('templating')->render($template, array(
-                    'paginator' => $this->paginator,
-                    'routeName' => (is_null($routeName)) ? '' : $routeName,
-                    'title' => $title,
-                    'tag' => (isset($options['tag'])) ? $options['tag'] : 'span',
-                    'paginatorId' => (is_null($paginatorId)) ? '' : $paginatorId,
-                    'disabledTitle' => (isset($options['disabledTitle'])) ? $options['disabledTitle'] : $title,
-                    'disabledClass' => (isset($options['disabledClass'])) ? $options['disabledClass'] : 'disabled'
-                )));
-    }
-
-    public function counter($format, $paginatorId)
-    {
-        $paginatorId = (is_null($paginatorId)) ? '' : $paginatorId;
-        $format = (is_null($format)) ? 'Page %page% of %pages%, showing %current% records out of %count% total, starting on record %start%, ending on %end%' : $format;
-        $fCounter = str_replace('%page%', $this->paginator->getCurrentPage($paginatorId), $format);
-        $fCounter = str_replace('%pages%', $this->paginator->getLastPage($paginatorId), $fCounter);
-        $fCounter = str_replace('%current%', $this->paginator->getItemsPerPage($paginatorId), $fCounter);
-        $fCounter = str_replace('%count%', $this->paginator->getTotalItems($paginatorId), $fCounter);
-        $fCounter = str_replace('%start%', $this->paginator->getOffset($paginatorId) + 1, $fCounter);
-        $end = $this->paginator->getOffset($paginatorId) + $this->paginator->getItemsPerPage($paginatorId);
-        $end = ($end < $this->paginator->getTotalItems($paginatorId)) ? $end : $this->paginator->getTotalItems($paginatorId);
-        $fCounter = str_replace('%end%', $end, $fCounter);
-        return trim($fCounter);
-    }
-
-    public function offset($index=0, $paginatorId, $startIndex)
-    {
-        $paginatorId = (is_null($paginatorId)) ? '' : $paginatorId;
-        return $this->paginator->getOffset($paginatorId) + $index + $startIndex;
+        return $this->container->get('templating')->render($view, $options);
     }
 
     public function getName()
--- Twig/Extension/PaginationExtension.php
+++ Twig/Extension/PaginationExtension.php
@@ -5,71 +5,81 @@ namespace Ideup\SimplePaginatorBundle\Twig\Extension;
 use Ideup\SimplePaginatorBundle\Templating\Helper\PagebarHelper as PagebarHelper;
 use Ideup\SimplePaginatorBundle\Paginator\Paginator as Paginator;
 
+class PaginationExtension extends \Twig_Extension
+{
 
-class PaginationExtension extends \Twig_Extension{
-	
-	protected $pagebar;	
-	
-	public function __construct(PagebarHelper $pagebar){
-		$this->pagebar = $pagebar;
-	}
+    protected $pagebar;
 
-	public function getFunctions() {
-		return array(
-			'pagination_first' => new \Twig_Function_Method($this, 'first', array('is_safe' => array('html'))),
-			'pagination_prev' => new \Twig_Function_Method($this, 'prev', array('is_safe' => array('html'))),
-			'pagination_numbers' => new \Twig_Function_Method($this, 'numbers', array('is_safe' => array('html'))),
-			'pagination_next' => new \Twig_Function_Method($this, 'next', array('is_safe' => array('html'))),
-			'pagination_last' => new \Twig_Function_Method($this, 'last', array('is_safe' => array('html'))),
-			'pagination_counter' => new \Twig_Function_Method($this, 'counter', array('is_safe' => array('html'))),
-			'pagination_offset' => new \Twig_Function_Method($this, 'offset', array('is_safe' => array('html'))),
-			'pagination_offset0' => new \Twig_Function_Method($this, 'offset0', array('is_safe' => array('html')))			
-		);
-	}
-	
-	public function first($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
-	{ 
-		return $this->pagebar->first($routeName, $title, $options, $paginatorId);
-	}
+    public function __construct(PagebarHelper $pagebar)
+    {
+        $this->pagebar = $pagebar;
+    }
 
-	public function prev($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
-	{ 
-		return $this->pagebar->prev($routeName, $title, $options, $paginatorId);
-	}
+    public function getFunctions()
+    {
+        return array(
+//            'pagination_first' => new \Twig_Function_Method($this, 'first', array('is_safe' => array('html'))),
+//            'pagination_prev' => new \Twig_Function_Method($this, 'prev', array('is_safe' => array('html'))),
+//            'pagination_numbers' => new \Twig_Function_Method($this, 'numbers', array('is_safe' => array('html'))),
+//            'pagination_next' => new \Twig_Function_Method($this, 'next', array('is_safe' => array('html'))),
+//            'pagination_last' => new \Twig_Function_Method($this, 'last', array('is_safe' => array('html'))),
+//            'pagination_counter' => new \Twig_Function_Method($this, 'counter', array('is_safe' => array('html'))),
+//            'pagination_offset' => new \Twig_Function_Method($this, 'offset', array('is_safe' => array('html'))),
+//            'pagination_offset0' => new \Twig_Function_Method($this, 'offset0', array('is_safe' => array('html'))),
+            'simple_paginator_render' => new \Twig_Function_Method($this, 'render', array('is_safe' => array('html'))),
+            
+        );
+    }
 
-	public function numbers($routeName=NULL, $options=NULL, $paginatorId=NULL)
-	{
-		return $this->pagebar->numbers($routeName, $options, $paginatorId);
-	}
-	
-	public function next($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
-	{
-		return $this->pagebar->next($routeName, $title, $options, $paginatorId);
-	}
-	
-	public function last($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
-	{ 
-		return $this->pagebar->last($routeName, $title, $options, $paginatorId);
-	}
-	
-	public function counter($format=NULL, $paginatorId=NULL)
-	{ 
-		return $this->pagebar->counter($format, $paginatorId);
-	}
-	
-	public function offset($index=NULL, $paginatorId=NULL)
-	{ 
-		return $this->pagebar->offset($index, $paginatorId, $startIndex=0);
-	}
-	
-	public function offset0($index=NULL, $paginatorId=NULL)
-	{ 
-		return $this->pagebar->offset($index, $paginatorId, $startIndex=1);
-	}
+//    public function first($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->first($routeName, $title, $options, $paginatorId);
+//    }
+//
+//    public function prev($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->prev($routeName, $title, $options, $paginatorId);
+//    }
+//
+//    public function numbers($routeName=NULL, $options=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->numbers($routeName, $options, $paginatorId);
+//    }
+//
+//    public function next($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->next($routeName, $title, $options, $paginatorId);
+//    }
+//
+//    public function last($routeName=NULL, $title=NULL, $options=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->last($routeName, $title, $options, $paginatorId);
+//    }
+//
+//    public function counter($format=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->counter($format, $paginatorId);
+//    }
+//
+//    public function offset($index=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->offset($index, $paginatorId, $startIndex = 0);
+//    }
+//
+//    public function offset0($index=NULL, $paginatorId=NULL)
+//    {
+//        return $this->pagebar->offset($index, $paginatorId, $startIndex = 1);
+//    }
 
-	public function getName() {
-		return 'pagination_extension';
-	}
+    public function render($id, $route, $options = array(), $view = null)
+    {
+        return $this->pagebar->render($id, $route, $options, $view);
+    }
+
+    public function getName()
+    {
+        return 'pagination_extension';
+    }
 
 }
 
