65d3f5bff81a3cbf537c250d6cad78464de5be03
javiacei <javier.aceituno@ideup.com>
2011-09-21 15:23:04 +0200

    Adding Test structure

---
+++ Tests/Paginator/PaginatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Ideup\SimplePaginatorBundle\Test\Paginator;
+
+class PaginatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testHi()
+    {
+        
+    }
+}
