2c48f7ac394948b7897f7ff14dea6005f2cb4bed
nass600 <ivelazquez85@gmail.com>
2012-07-30 11:59:34 +0200

    Added native support for Doctrine\Common\Collections\ArrayCollection

---
+++ Paginator/Adapter/Doctrine/Common/Collections/ArrayCollectionAdapter.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Ideup\SimplePaginatorBundle\Paginator\Adapter;
+
+use
+    Ideup\SimplePaginatorBundle\Paginator\Adapter\AdapterInterface
+;
+
+/**
+ * ArrayCollectionAdapter
+ *
+ * @package IdeupSimplePaginatorBundle
+ * @subpackage Adapter
+ * @author Ignacio Velazquez <ivelazquez85@gmail.com>
+
+ */
+class ArrayCollectionAdapter implements AdapterInterface
+{    
+    protected $collection;
+    protected $offset;
+    protected $length;
+
+    public function __construct(\Doctrine\Common\Collections\ArrayCollection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function getTotalResults()
+    {
+        return $this->collection->count();
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function setOffset($offset)
+    {
+        $this->offset = $offset;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function setLength($length)
+    {
+        $this->length = $length;
+        return $this;
+    }
+
+    /**
+     * {@inheritdoc }
+     */
+    public function getResult()
+    {
+        return $this->collection->slice($this->offset, $this->length);
+    }
+}
